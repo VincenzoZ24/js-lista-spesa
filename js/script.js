ea6be8d833d0555fcb579271b1bc767394b3811a
VincenzoZ24 <zitov287@gmail.com>
2022-10-27 14:55:53 +0200

    esercizio terminato con ciclo while

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ const arrListaSpesa = [];
 let eleText = document.querySelector(".text_list");
 let eleForm = document.querySelector("form");
 let listaSpesa = document.querySelector("lista_spesa");
-for (i = 0; i < arrListaSpesa.length; i++) {
-    listaSpesa.innerHTML += `<li>${arrListaSpesa[i]}</li>`
+// for (i = 0; i < arrListaSpesa.length; i++) {
+//     listaSpesa.innerHTML += `<li>${arrListaSpesa[i]}</li>`
 
-}
+// }
+i=0;
+while (i < arrListaSpesa.length) {
+    listaSpesa.innerHTML += `<li>${arrListaSpesa[i]}</li>`
+    i++;
+};
 
 eleForm.addEventListener('submit', function (event) {
     event.preventDefault();
