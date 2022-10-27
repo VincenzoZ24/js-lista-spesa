1f7e54a05317db2b03bb1a84a266350d2a9f9230
VincenzoZ24 <zitov287@gmail.com>
2022-10-27 14:39:48 +0200

    definisco evento

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ const arrListaSpesa = [];
 let eleText = document.querySelector(".text_list");
 let eleForm = document.querySelector("form");
 let listaSpesa = document.querySelector("lista_spesa");
- for(i = 0; i < arrListaSpesa.length; i++){
-    
- }
+for (i = 0; i < arrListaSpesa.length; i++) {
+    listaSpesa.innerHTML += `<li>${arrListaSpesa[i]}</li>`
+
+}
+
+eleForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    arrListaSpesa.push(eleText.value);
+
+})
