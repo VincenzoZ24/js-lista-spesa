eda0cc334d1c593ebbfc611711851104927c2edf
VincenzoZ24 <zitov287@gmail.com>
2022-10-27 14:29:46 +0200

    seleziono variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-const arrListaSpesa = []
+const arrListaSpesa = [];
+
+let eleText = document.querySelector(".text_list");
+let eleForm = document.querySelector("form");
+let listaSpesa = document.querySelector("lista_spesa");
+ for(i = 0; i < arrListaSpesa.length; i++){
+    
+ }
